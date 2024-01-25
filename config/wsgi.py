5359a5df3f6c40fac812cffdb13bcdf34c3eb65c
paullee714 <paul_lee@kakao.com>
2024-01-25 11:42:20 +0900

    feat: add env configuration in sgi

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "config.settings." + os.getenv("DJANGO_CONFIG", "dev")
+)
 application = get_wsgi_application()
